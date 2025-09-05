aee974c08ce1385972d6ecd1041c5c57ed281f88
geraldoMatheus21 <geraldomatheus2@gmail.com>
2025-09-05 12:30:20 -0300

    Exercicio1

---
+++ exerciciosLista/src/exerciciosLista/Exercicio1.java
@@ -0,0 +1,21 @@
+package exerciciosLista;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		
+		list.add("Azul");
+		list.add("Amarelo");
+		list.add("Preto");
+		list.add("Verde");
+		
+		System.out.println(list);
+	
+	}
+
+}
