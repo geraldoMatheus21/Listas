3b304d1b8312ff3af8c35b0f20fc02194532fed6
geraldoMatheus21 <geraldomatheus2@gmail.com>
2025-09-05 12:43:35 -0300

    Exercicio2

---
+++ exerciciosLista/src/exerciciosLista/Exercicios2.java
@@ -0,0 +1,34 @@
+package exerciciosLista;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercicios2 {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		
+		String[] pessoas = {
+			    "Ana Silva",
+			    "Carlos Oliveira", 
+			    "Maria Santos",
+			    "João Pereira",
+			    "Juliana Costa",
+			    "Pedro Almeida",
+			    "Fernanda Lima",
+			    "Ricardo Souza",
+			    "Amanda Rodrigues",
+			    "Paulo Cardoso"
+			};
+		
+		for(int i = 0;i<pessoas.length; i++) {
+			list.add(pessoas[i]);
+		}
+		
+		for(String o : list) {
+			System.out.println(o);
+		}
+		
+	}
+
+}
